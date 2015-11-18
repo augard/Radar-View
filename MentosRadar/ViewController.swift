1096484d27ac2577e2328caeec53701c3c597263
Lukas Foldyna <lukas.foldyna@arat.cz>
2015-11-18 10:40:33 +0100

    Initial Commit

---
+++ MentosRadar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MentosRadar
+//
+//  Created by Lukáš Foldýna on 18/11/15.
+//  Copyright © 2015 The Funtasty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
